a86e959e8286513c64b6fb76203919983ab9bcd8
NoDocCat <nodoccat@outlook.com>
2021-02-18 23:16:52 +0800

    feat: 完成 BatteryManager

---
+++ src/BatteryManager.spec.ts
@@ -0,0 +1,91 @@
+import { BatteryManager } from "./BatteryManager";
+
+test("应正确初始化预设值", () => {
+  const manager = new BatteryManager();
+
+  expect(manager.charging).toBe(true);
+  expect(manager.chargingTime).toBe(Infinity);
+  expect(manager.dischargingTime).toBe(Infinity);
+  expect(manager.level).toBe(1);
+});
+
+test("应正确处理事件注册, 移除和触发", () => {
+  const manager = new BatteryManager();
+  const listener1 = jest.fn();
+  const listener2 = jest.fn();
+
+  manager.addEventListener("levelchange", listener1, false);
+  manager.addEventListener("levelchange", listener2, false);
+  manager.dispatchEvent(new Event("levelchange"));
+  expect(listener1).toBeCalledTimes(1);
+  expect(listener2).toBeCalledTimes(1);
+
+  manager.removeEventListener("levelchange", listener1, false);
+  manager.dispatchEvent(new Event("levelchange"));
+  expect(listener1).toBeCalledTimes(1);
+  expect(listener2).toBeCalledTimes(2);
+});
+
+test("应在事件回调中包含当前电池状态", done => {
+  const manager = new BatteryManager();
+
+  manager.addEventListener("levelchange", event => {
+    const target = event.target as BatteryManager;
+    expect(target.charging).toBe(true);
+    done();
+  });
+
+  manager.dispatchEvent(new Event("levelchange"));
+});
+
+test("onchargingchange", done => {
+  const manager = new BatteryManager();
+
+  manager.onchargingchange = event => {
+    const target = event.target as BatteryManager;
+    expect(event.type).toBe("chargingchange");
+    expect(target.charging).toBe(true);
+    done();
+  };
+
+  manager.dispatchEvent(new Event("chargingchange"));
+});
+
+test("onchargingtimechange", done => {
+  const manager = new BatteryManager();
+
+  manager.onchargingtimechange = event => {
+    const target = event.target as BatteryManager;
+    expect(event.type).toBe("chargingtimechange");
+    expect(target.charging).toBe(true);
+    done();
+  };
+
+  manager.dispatchEvent(new Event("chargingtimechange"));
+});
+
+test("ondischargingtimechange", done => {
+  const manager = new BatteryManager();
+
+  manager.ondischargingtimechange = event => {
+    const target = event.target as BatteryManager;
+    expect(event.type).toBe("dischargingtimechange");
+    expect(target.charging).toBe(true);
+    done();
+  };
+
+  manager.dispatchEvent(new Event("dischargingtimechange"));
+});
+
+test("onlevelchange", done => {
+  const manager = new BatteryManager();
+
+  manager.onlevelchange = event => {
+    const target = event.target as BatteryManager;
+    expect(event.type).toBe("levelchange");
+    expect(target.charging).toBe(true);
+    done();
+  };
+
+  manager.dispatchEvent(new Event("levelchange"));
+});
---
+++ src/BatteryManager.ts
@@ -0,0 +1,76 @@
+export class BatteryManager extends EventTarget {
+  charging: boolean;
+  chargingTime: number;
+  dischargingTime: number;
+  level: number;
+  onchargingchange: EventListener | null;
+  onchargingtimechange: EventListener | null;
+  ondischargingtimechange: EventListener | null;
+  onlevelchange: EventListener | null;
+
+  constructor() {
+    super();
+
+    // default value
+    this.charging = true;
+    this.chargingTime = Infinity;
+    this.dischargingTime = Infinity;
+    this.level = 1;
+    this.onchargingchange = null;
+    this.onchargingtimechange = null;
+    this.ondischargingtimechange = null;
+    this.onlevelchange = null;
+  }
+
+  dispatchEvent(event: Event): boolean {
+    if (typeof this.onchargingchange === "function" && event.type === "chargingchange") {
+      const eventData: Event = {
+        ...new Event("chargingchange"),
+        type: "chargingchange",
+        currentTarget: this,
+        srcElement: this,
+        target: this,
+      };
+
+      this.onchargingchange.call(this, eventData);
+    }
+
+    if (typeof this.onchargingtimechange === "function" && event.type === "chargingtimechange") {
+      const eventData: Event = {
+        ...new Event("chargingtimechange"),
+        type: "chargingtimechange",
+        currentTarget: this,
+        srcElement: this,
+        target: this,
+      };
+
+      this.onchargingtimechange.call(this, eventData);
+    }
+
+    if (typeof this.ondischargingtimechange === "function" && event.type === "dischargingtimechange") {
+      const eventData: Event = {
+        ...new Event("dischargingtimechange"),
+        type: "dischargingtimechange",
+        currentTarget: this,
+        srcElement: this,
+        target: this,
+      };
+
+      this.ondischargingtimechange.call(this, eventData);
+    }
+
+    if (typeof this.onlevelchange === "function" && event.type === "levelchange") {
+      const eventData: Event = {
+        ...new Event("levelchange"),
+        type: "levelchange",
+        currentTarget: this,
+        srcElement: this,
+        target: this,
+      };
+
+      this.onlevelchange.call(this, eventData);
+    }
+
+    return super.dispatchEvent(event);
+  }
+}
