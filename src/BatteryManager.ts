072431bc7cd93b2745370bc339c7d0fd879611eb
NoDocCat <nodoccat@outlook.com>
2023-02-27 21:32:21 +0800

    feat: 完成事件核心

--- src/BatteryManager.ts
+++ src/BatteryManager.ts
@@ -4,3 +4,40 @@ export interface BatteryInfo {
   dischargingTime: number
   level: number
 }
+
+export class BatteryManager extends EventTarget implements BatteryInfo {
+  charging: boolean
+  chargingTime: number
+  dischargingTime: number
+  level: number
+
+  onchargingchange: EventListener | null = null
+  onchargingtimechange: EventListener | null = null
+  ondischargingtimechange: EventListener | null = null
+  onlevelchange: EventListener | null = null
+
+  constructor (preset: BatteryInfo) {
+    super()
+    this.charging = preset.charging
+    this.chargingTime = preset.chargingTime
+    this.dischargingTime = preset.dischargingTime
+    this.level = preset.level
+  }
+
+  dispatchEvent (event: Event) {
+    if (event.type === 'chargingchange') {
+      this.onchargingchange?.({ ...event, currentTarget: this, target: this })
+    }
+    if (event.type === 'chargingtimechange') {
+      this.onchargingtimechange?.({ ...event, currentTarget: this, target: this })
+    }
+    if (event.type === 'dischargingtimechange') {
+      this.ondischargingtimechange?.({ ...event, currentTarget: this, target: this })
+    }
+    if (event.type === 'levelchange') {
+      this.onlevelchange?.({ ...event, currentTarget: this, target: this })
+    }
+
+    return super.dispatchEvent(event)
+  }
+}
